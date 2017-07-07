8efb3dcb06d160dbc7f4cf9a7c5aa0d4ac4be724
Ajinkya <albus702@gmail.com>
2017-07-07 17:26:21 +0530

    added TODO's

---
+++ handwrittenDigits.py
@@ -0,0 +1,17 @@
+############################################################################################################################
+# TODO:
+
+# 1. Download MNIST Dataset
+# 2. Setup Neural Network with required number of layers & nodes and then train it
+# 3. Check output for 1 image
+# 4. Feed the Test Dataset to the trained Neural Network & check its accuracy
+
+
+############################################################################################################################
+
+def load_dataset():
+    print "********** Started Loading Data **********"
+    print "********** Completed Loading Data **********"
+
+
+load_dataset()
