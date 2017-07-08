2823b113377d81ad70426b39f84c8c87a1cfea93
Ajinkya <albus702@gmail.com>
2017-07-08 16:02:31 +0530

    theory, reindentation

--- handwrittenDigits.py
+++ handwrittenDigits.py
@@ -6,7 +6,8 @@
 # 3. Check output for 1 image
 # 4. Feed the Test Dataset to the trained Neural Network & check its accuracy
 
-
+# Higher dimensional array are often called tensors. We use Theano to work with tensors.
+# Lasagne is built on top of Theano and is used to build Neural Networks.
 ############################################################################################################################
 import urllib
 import gzip
@@ -14,12 +15,13 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def load_dataset():
-    print "********** Started Loading Data **********"
+    # print "********** Started Loading Data **********"
 
     def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
         # print "********** Started Downloading Data **********"
-        urllib.urlretrieve(source+filename,  filename)
+        urllib.urlretrieve(source + filename, filename)
         # print "********** Completed Downloading Data **********"
 
     def load_mnist_images(filename):
@@ -30,7 +32,7 @@ def load_dataset():
             # print f
             data = np.frombuffer(f.read(), np.uint8, offset=16)
             # print data
-            data = data.reshape(-1,1,28,28)
+            data = data.reshape(-1, 1, 28, 28)
             return data / np.float32(256)
 
     def load_mnist_labels(filename):
@@ -45,11 +47,14 @@ def load_dataset():
     Y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
     X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
     Y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
-
-    print "********** Completed Loading Data **********"
+    # print "********** Completed Loading Data **********"
     return X_train, Y_train, X_test, Y_test
 
 
 X_train, Y_train, X_test, Y_test = load_dataset()
 
-plt.show(plt.imshow(X_train[1][0]))
+
+# plt.show(plt.imshow(X_train[1][0]))
+
+def build_NN(input_var=None):
+    return
